ee2df3ec04ef0dc6b1a8abe7fd4eb3504cca4bd6
thi64146 <thi64146@adobe.com>
2022-02-15 17:16:30 -0700

    initial zpattern

---
+++ blocks/z-pattern/z-pattern.js
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/*
+ * Z-Pattern - v0.0.1
+ */
+
+function isOdd(num) { return num % 2 ? 'z-row-odd' : 'z-row-even'; }
+
+function decorateButtons(el) {
+  const buttons = el.querySelectorAll('em a, strong a');
+  buttons.forEach((button) => {
+    const parent = button.parentElement;
+    const buttonType = parent.nodeName === 'STRONG' ? 'blue' : 'outline';
+    button.classList.add('con-button', buttonType);
+    parent.insertAdjacentElement('afterend', button);
+    parent.remove();
+  });
+  if (buttons.length > 0) {
+    buttons[0].closest('p').classList.add('action-area');
+  }
+}
+
+function decorateText(el, size) {
+  const headings = el.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  const heading = headings[headings.length - 1];
+  if (!size) {
+    heading.classList.add('heading-XS');
+    heading.nextElementSibling.classList.add('body-S');
+    if (heading.previousElementSibling) {
+      heading.previousElementSibling.classList.add('detail-M');
+    }
+  }
+
+  if (size === 'm') {
+    heading.classList.add('heading-M');
+    heading.nextElementSibling.classList.add('body-S');
+    if (heading.previousElementSibling) {
+      heading.previousElementSibling.classList.add('detail-M');
+    }
+  }
+  if (size === 'l') {
+    heading.classList.add('heading-XL');
+    heading.nextElementSibling.classList.add('body-M');
+    if (heading.previousElementSibling) {
+      heading.previousElementSibling.classList.add('detail-L');
+    }
+  }
+}
+
+function getSize(el) {
+  if (el.classList.contains('medium')) {
+    return 'm';
+  }else if (el.classList.contains('large')) {
+    return 'l';
+  }else{
+    return null;
+  }
+}
+
+export default function init(block)  {
+
+    const size = getSize(block);
+    const h2 = block.querySelector('h2');
+    if (h2) {
+        h2.parentElement.parentElement.classList.add('z-pattern-heading');
+    }
+    const zRows = block.querySelectorAll(':scope > div:not([class])');
+    zRows.forEach((row, idx) => {
+        row.classList.add(isOdd(idx));
+        const text = row.querySelector('h1, h2, h3, h4, h5, h6').closest('div');
+        text.classList.add('text');
+        const image = row.querySelector(':scope > div:not([class])');
+        if (image) {
+          image.classList.add('image');
+        }
+        decorateButtons(text);
+        decorateText(text, size);
+    });
+}
